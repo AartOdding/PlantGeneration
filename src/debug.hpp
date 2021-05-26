0ebd5ae5d06199b1d07a7c6701a2366876d948c2
Aart Stuurman <aart.stuurman@knmi.nl>
2021-05-26 23:32:44 +0200

    Update types. Add printing to debug.hpp

--- src/debug.hpp
+++ src/debug.hpp
@@ -18,19 +18,9 @@ namespace Mb4 {
 
 using AssertError = std::runtime_error;
 
-void ThrowAssertFailure(std::experimental::source_location const& source_location)
-{
-    std::string errstr = "Assert failed!\nFile: ";
-    errstr.append(source_location.file_name());
-    errstr.append("\nFunction: ");
-    errstr.append(source_location.function_name());
-    errstr.append("\nLine: ");
-    errstr.append(std::to_string(source_location.line()));
-    errstr.append("\nColumn: ");
-    errstr.append(std::to_string(source_location.column()));
-    
-    throw AssertError(errstr);
-}
+void DebugPrint(std::string const& str);
+
+void ThrowAssertFailure(std::experimental::source_location const& source_location);
 
 template <typename T1, typename T2>
 void AssertEqual(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
@@ -90,6 +80,4 @@ void AssertSmallerEqual(T1 const& a, T2 const& b, std::experimental::source_loca
             ThrowAssertFailure(source_location);
         }
     }
-}
-
 }
--- src/types.hpp
+++ src/types.hpp
@@ -1,4 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 using f32 = float;
 using f64 = double;
+using u32 = std::uint32_t;
