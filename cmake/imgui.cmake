cc092d5f3b2b897acb9b54cadf5313f10d8c813a
Aart <aart_odding@hotmail.com>
2021-05-27 01:27:45 +0200

    build imgui from cmake script

---
+++ cmake/imgui.cmake
@@ -0,0 +1,24 @@
+
+add_library(imgui STATIC
+    dependencies/imgui/imconfig.h
+    dependencies/imgui/imgui.cpp
+    dependencies/imgui/imgui.h
+    dependencies/imgui/imgui_demo.cpp
+    dependencies/imgui/imgui_draw.cpp
+    dependencies/imgui/imgui_internal.h
+    dependencies/imgui/imgui_tables.cpp
+    dependencies/imgui/imgui_widgets.cpp
+    dependencies/imgui/imstb_rectpack.h
+    dependencies/imgui/imstb_textedit.h
+    dependencies/imgui/imstb_truetype.h
+    dependencies/imgui/backends/imgui_impl_glfw.cpp
+    dependencies/imgui/backends/imgui_impl_glfw.h
+    dependencies/imgui/backends/imgui_impl_opengl3.cpp
+    dependencies/imgui/backends/imgui_impl_opengl3.h
+)
+
+target_include_directories(imgui PUBLIC "${CMAKE_SOURCE_DIR}/dependencies/imgui")
+target_include_directories(imgui PUBLIC "${CMAKE_SOURCE_DIR}/dependencies/imgui/backends")
+
+target_compile_definitions(imgui PUBLIC IMGUI_IMPL_OPENGL_LOADER_CUSTOM="external/glad.h")
+target_link_libraries(imgui PRIVATE raylib)
