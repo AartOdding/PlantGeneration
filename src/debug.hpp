195d13e26c0f1526df68c49066dbc1e78a258d44
Aart Stuurman <aartstuurman@hotmail.com>
2021-05-27 15:01:47 +0200

    Rework debug.hpp to work on systems without source_location.

--- src/debug.hpp
+++ src/debug.hpp
@@ -6,9 +6,9 @@
 #pragma once
 
 #include <stdexcept>
-#include <experimental/source_location>
 
-namespace Mb4 {
+namespace Mb4
+{
 
 #ifdef MB4_PREPROCESSOR_DEBUG
     constexpr static bool MB4_DEBUG = true;
@@ -18,66 +18,156 @@ namespace Mb4 {
 
 using AssertError = std::runtime_error;
 
-void DebugPrint(std::string const& str);
+void DebugPrint(std::string const &str);
 
-void ThrowAssertFailure(std::experimental::source_location const& source_location);
-
-template <typename T1, typename T2>
-void AssertEqual(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
-{
-    if constexpr (MB4_DEBUG)
-    {
-        if (!(a == b))
-        {
-            ThrowAssertFailure(source_location);
-        }
-    }
 }
 
-template <typename T1, typename T2>
-void AssertLarger(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
-{
-    if constexpr (MB4_DEBUG)
-    {
-        if (!(a > b))
-        {
-            ThrowAssertFailure(source_location);
-        }
-    }
-}
+#if __has_include(<experimental/source_location>)
+    #include <experimental/source_location>
 
-template <typename T1, typename T2>
-void AssertLargerEqual(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
-{
-    if constexpr (MB4_DEBUG)
-    {
-        if (!(a >= b))
-        {
-            ThrowAssertFailure(source_location);
-        }
-    }
-}
+    #define Mb4_AssertEqual(a, b) Mb4::AssertEqual(a, b)
+    #define Mb4_AssertLarger(a, b) Mb4::AssertLarger(a, b)
+    #define Mb4_AssertLargerEqual(a, b) Mb4::AssertLargerEqual(a, b)
+    #define Mb4_AssertSmaller(a, b) Mb4::AssertSmaller(a, b)
+    #define Mb4_AssertSmallerEqual(a, b) Mb4::AssertSmallerEqual(a, b)
 
-template <typename T1, typename T2>
-void AssertSmaller(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
-{
-    if constexpr (MB4_DEBUG)
+    namespace Mb4
     {
-        if (!(a < b))
-        {
-            ThrowAssertFailure(source_location);
-        }
-    }
-}
 
-template <typename T1, typename T2>
-void AssertSmallerEqual(T1 const& a, T2 const& b, std::experimental::source_location source_location = std::experimental::source_location::current())
-{
-    if constexpr (MB4_DEBUG)
+    void ThrowAssertFailure(std::experimental::source_location const &source_location);
+
+    template <typename T1, typename T2>
+    void AssertEqual(T1 const &a, T2 const &b, std::experimental::source_location source_location = std::experimental::source_location::current())
     {
-        if (!(a <= b))
+        if constexpr (MB4_DEBUG)
         {
-            ThrowAssertFailure(source_location);
+            if (!(a == b))
+            {
+                ThrowAssertFailure(source_location);
+            }
         }
     }
-}
+
+    template <typename T1, typename T2>
+    void AssertLarger(T1 const &a, T2 const &b, std::experimental::source_location source_location = std::experimental::source_location::current())
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a > b))
+            {
+                ThrowAssertFailure(source_location);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertLargerEqual(T1 const &a, T2 const &b, std::experimental::source_location source_location = std::experimental::source_location::current())
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a >= b))
+            {
+                ThrowAssertFailure(source_location);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertSmaller(T1 const &a, T2 const &b, std::experimental::source_location source_location = std::experimental::source_location::current())
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a < b))
+            {
+                ThrowAssertFailure(source_location);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertSmallerEqual(T1 const &a, T2 const &b, std::experimental::source_location source_location = std::experimental::source_location::current())
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a <= b))
+            {
+                ThrowAssertFailure(source_location);
+            }
+        }
+    }
+
+    }
+#else  // __has_include(<experimental/source_location>)
+    #define Mb4_AssertEqual(a, b) Mb4::AssertEqual(a, b, __func__, __FILE__, __LINE__)
+    #define Mb4_AssertLarger(a, b) Mb4::AssertLarger(a, b, __func__, __FILE__, __LINE__)
+    #define Mb4_AssertLargerEqual(a, b) Mb4::AssertLargerEqual(a, b, __func__, __FILE__, __LINE__)
+    #define Mb4_AssertSmaller(a, b) Mb4::AssertSmaller(a, b, __func__, __FILE__, __LINE__)
+    #define Mb4_AssertSmallerEqual(a, b) Mb4::AssertSmallerEqual(a, b, __func__, __FILE__, __LINE__)
+
+    namespace Mb4
+    {
+
+    void ThrowAssertFailure(std::string const& function, std::string const& file, int line);
+
+    template <typename T1, typename T2>
+    void AssertEqual(T1 const &a, T2 const &b, std::string const& function, std::string const& file, int line)
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a == b))
+            {
+                ThrowAssertFailure(function, file, line);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertLarger(T1 const &a, T2 const &b, std::string const& function, std::string const& file, int line)
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a > b))
+            {
+                ThrowAssertFailure(function, file, line);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertLargerEqual(T1 const &a, T2 const &b, std::string const& function, std::string const& file, int line)
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a >= b))
+            {
+                ThrowAssertFailure(function, file, line);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertSmaller(T1 const &a, T2 const &b, std::string const& function, std::string const& file, int line)
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a < b))
+            {
+                ThrowAssertFailure(function, file, line);
+            }
+        }
+    }
+
+    template <typename T1, typename T2>
+    void AssertSmallerEqual(T1 const &a, T2 const &b, std::string const& function, std::string const& file, int line)
+    {
+        if constexpr (MB4_DEBUG)
+        {
+            if (!(a <= b))
+            {
+                ThrowAssertFailure(function, file, line);
+            }
+        }
+    }
+
+    }
+#endif // __has_include(<experimental/source_location>)
--- src/main.cpp
+++ src/main.cpp
@@ -7,100 +7,114 @@
 #include <cmrc/cmrc.hpp>
 #include <iostream>
 
-CMRC_DECLARE(resources);
+#include "debug.hpp"
 
+CMRC_DECLARE(resources);
 
 int main()
 {
-    auto fs = cmrc::resources::get_filesystem();
-    auto f = fs.open("shaders/test.frag");
-
-    std::string frag = std::string(f.begin(), f.end());
-    std::cout << frag << std::endl;
-
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    SetConfigFlags(FLAG_VSYNC_HINT);
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(800, 450, "Aart & Aart 4Mb Jam");
-
-    ShowCursor();
-    GetWindowHandle();
-
-    SetExitKey(0); // Removes ESC to exit
-    SetTargetFPS(60);
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui::StyleColorsDark();
-    
-    ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)GetGLFWWindowHandle(), true);
-    ImGui_ImplOpenGL3_Init("#version 330");
-
-    Camera camera{};
-    camera.position = { 0.0f, 1.8f, 6.0f };    // Camera position
-    camera.target = { 0.0f, 0.5f, 0.0f };      // Camera looking at point
-    camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                       // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;    // Camera mode type
-
-    auto mesh = GenMeshCube(1, 1, 1);
-    auto model = LoadModelFromMesh(mesh);
-
-    bool isPaused = true;
-
-    while (!WindowShouldClose())
+    try
     {
-        if (IsMouseButtonReleased(0)) // Only fires on the frame where the button is released
+        /*
+            Like this now. No namespace, :(
+            Mb4_AssertEqual(true, false);
+            Mb4_AssertLarger(1, 1);
+            Mb4_AssertLargerEqual(0, 1);
+            Mb4_AssertSmaller(1, 1);
+            Mb4_AssertSmallerEqual(1, 0);
+        */
+
+        auto fs = cmrc::resources::get_filesystem();
+        auto f = fs.open("shaders/test.frag");
+
+        std::string frag = std::string(f.begin(), f.end());
+        std::cout << frag << std::endl;
+
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        SetConfigFlags(FLAG_MSAA_4X_HINT);
+        SetConfigFlags(FLAG_VSYNC_HINT);
+        SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+        InitWindow(800, 450, "Aart & Aart 4Mb Jam");
+
+        SetExitKey(0); // Removes ESC to exit
+        SetTargetFPS(60);
+
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)GetGLFWWindowHandle(), true);
+        ImGui_ImplOpenGL3_Init("#version 330");
+
+        Camera camera{};
+        camera.position = { 0.0f, 1.8f, 6.0f };    // Camera position
+        camera.target = { 0.0f, 0.5f, 0.0f };      // Camera looking at point
+        camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+        camera.fovy = 45.0f;                       // Camera field-of-view Y
+        camera.projection = CAMERA_PERSPECTIVE;    // Camera mode type
+
+        auto mesh = GenMeshCube(1, 1, 1);
+        auto model = LoadModelFromMesh(mesh);
+
+        bool isPaused = true;
+
+        while (!WindowShouldClose())
         {
-            SetCameraMode(camera, CAMERA_FIRST_PERSON);
-            isPaused = false;
+            if (IsMouseButtonReleased(0)) // Only fires on the frame where the button is released
+            {
+                SetCameraMode(camera, CAMERA_FIRST_PERSON);
+                isPaused = false;
+            }
+            if (IsKeyPressed(KEY_ESCAPE) || IsKeyPressed(KEY_P)) // Only fires on the frame where a key is released
+            {
+                SetCameraMode(camera, CAMERA_CUSTOM);
+                isPaused = true;
+            }
+
+            UpdateCamera(&camera);
+
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+            DrawModel(model, { 0, 0, 0 }, 0.5, RED);
+            DrawGrid(40, 10.0f);
+
+            EndMode3D();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            bool show = true;
+            ImGui::ShowDemoWindow(&show);
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            if (isPaused)
+            {
+                DrawText("Click to start", GetScreenWidth() / 2, GetScreenHeight() / 2, 30, BLACK);
+            }
+
+            EndDrawing();
         }
-        if (IsKeyPressed(KEY_ESCAPE) || IsKeyPressed(KEY_P)) // Only fires on the frame where a key is released
-        {
-            SetCameraMode(camera, CAMERA_CUSTOM);
-            isPaused = true;
-        }
 
-        UpdateCamera(&camera);
+        UnloadModel(model);
 
-        BeginDrawing();
+        CloseWindow();
 
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-        DrawModel(model, { 0, 0, 0 }, 0.5, RED);
-        DrawGrid(40, 10.0f);
-
-        EndMode3D();
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        bool show = true;
-        ImGui::ShowDemoWindow(&show);
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-
-        if (isPaused)
-        {
-            DrawText("Click to start", GetScreenWidth() / 2, GetScreenHeight() / 2, 30, BLACK);
-        }
-
-        EndDrawing();
+        return 0;
     }
-
-    UnloadModel(model);
-
-    CloseWindow();
-
-    return 0;
+    catch(Mb4::AssertError err)
+    {
+        std::cout << err.what() << std::endl;
+        return 1;
+    }
 }
