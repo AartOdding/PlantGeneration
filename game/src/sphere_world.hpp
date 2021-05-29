9b8895d1cd63ef380b88687d8ff84c8416a520ee
Aart Stuurman <aartstuurman@hotmail.com>
2021-05-30 01:13:53 +0200

    Create sphere world class that can create a geodesic octahedron by subdividing the triangles of a tetrahedron

---
+++ game/src/sphere_world.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "types.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+#include <raylib.h>
+
+namespace Mb4
+{
+
+class SphereWorld
+{
+public:
+	struct Triangle
+	{
+	public:
+		u32 const index1;
+		u32 const index2;
+		u32 const index3;
+
+		Triangle(u32 const index1, u32 const index2, u32 const index3);
+	};
+private:
+	SphereWorld(std::tuple<std::vector<glm::fvec3>, std::vector<Triangle>>);
+
+	/* Create a geodesic octahedron by subdividing the triangles of a tetrahedron */
+	std::tuple<std::vector<glm::fvec3>, std::vector<Triangle>> CreateGeodesicPolyhedron(u32 divisions);
+
+	std::tuple<std::vector<glm::fvec3>, std::vector<Triangle>> SubDivide(std::vector<glm::fvec3> const& points, std::vector<Triangle> const& triangles);
+public:
+	std::vector<glm::fvec3> const points;
+	std::vector<Triangle> const triangles;
+
+	/*
+	 * @param divisions start with a tetrahedral and divide triangles this many times.
+	 */
+	SphereWorld(u32 divisions);
+};
+
+}
