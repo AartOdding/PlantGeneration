a7e428cc103ebd2601133e15a88c5f4f4dbfb477
Aart <aart_odding@hotmail.com>
2021-06-05 01:14:14 +0200

    Moved bunch of code out of main

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -23,20 +23,92 @@
 
 CMRC_DECLARE(resources);
 
-float camera_minmax_y = 0.3;
-float camera_speed_sideways = 0.007;
-float camera_speed_updown = 0.3;
 
+struct EditorConfig
+{
+    float camera_minmax_y = 0.3;
+    float camera_speed_sideways = 0.007;
+    float camera_speed_updown = 0.3;
+
+    bool orbit_mode = true;
+    bool editing_mode = true;
+
+    int last_width = 1080;
+    int last_height = 1350;
+};
+
+
+struct CameraState
+{
+    float camera_distance = 7;
+    float camera_rotation_sideways = 0;
+    float camera_rotation_updown = 0.3;
+
+    Vector2 previous_mouse_pos{ -1, -1 };
+};
+
+void UpdateCameraState(Camera& camera, CameraState& camera_state, EditorConfig& editor_config)
+{
+    if (IsKeyDown(KEY_DOWN))
+    {
+        camera.target.y += 0.02;
+    }
+    if (IsKeyDown(KEY_UP))
+    {
+        camera.target.y -= 0.02;
+    }
+    if (GetMouseWheelMove() != 0)
+    {
+        const float desired_distance = camera_state.camera_distance * std::pow(0.95, GetMouseWheelMove());
+        camera_state.camera_distance = std::clamp<float>(desired_distance, 0.1, 1000);
+    }
+
+    if (editor_config.orbit_mode)
+    {
+        camera_state.camera_rotation_sideways += editor_config.camera_speed_sideways;
+        camera_state.camera_rotation_updown = editor_config.camera_minmax_y * sin(editor_config.camera_speed_updown * GetTime());
+    }
+    else
+    {
+        // Manual controls:
+
+        if (IsMouseButtonDown(1))
+        {
+            if (camera_state.previous_mouse_pos.x == -1 && camera_state.previous_mouse_pos.y == -1)
+            {
+                camera_state.previous_mouse_pos = GetMousePosition();
+            }
+            auto movement = Vector2Subtract(GetMousePosition(), camera_state.previous_mouse_pos);
+
+            camera_state.camera_rotation_sideways += -movement.x / (GetScreenWidth() / 5.0f);
+            camera_state.camera_rotation_updown += movement.y / (GetScreenHeight() / 5.0f);
+        }
+    }
+
+    const Quaternion sideways_quaternion = QuaternionFromEuler(0, camera_state.camera_rotation_sideways, 0);
+    const Vector3 pos_xz = Vector3RotateByQuaternion({ 0, 0, camera_state.camera_distance }, sideways_quaternion);
+    const Vector3 tangent = Vector3RotateByQuaternion(pos_xz, QuaternionFromEuler(0, -glm::half_pi<float>(), 0));
+    const Quaternion updown_quaternion = QuaternionFromAxisAngle(tangent, camera_state.camera_rotation_updown);
+    const Vector3 camera_position = Vector3RotateByQuaternion(pos_xz, updown_quaternion);
+
+    camera.position = Vector3Add(camera.target, camera_position);
+
+    camera_state.previous_mouse_pos = GetMousePosition();
+}
 
 
 int main()
 {
+    EditorConfig editor_config;
+    CameraState camera_state;
+
     Dandelion2 current_tree;
     auto lsystem = current_tree.Generate();
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     SetConfigFlags(FLAG_VSYNC_HINT);
-    //SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    
     InitWindow(1080, 1350, "Aart & Aart 4Mb Jam");
 
     SetTargetFPS(30);
@@ -56,69 +128,29 @@ int main()
     camera.fovy = 45.0f;                       // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;    // Camera mode type
 
-    float camera_distance = 7;
-    float camera_rotation_sideways = 0;
-    float camera_rotation_updown = 0.3;
-
-    Vector2 previous_mouse_pos{ -1, -1 };
-    bool render_gui = true;
-    bool orbit_mode = true;
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_DOWN))
-        {
-            camera.target.y += 0.02;
-        }
-        if (IsKeyDown(KEY_UP))
-        {
-            camera.target.y -= 0.02;
-        }
-        if (GetMouseWheelMove() != 0)
-        {
-            const float desired_distance = camera_distance * std::pow(0.9, GetMouseWheelMove());
-            camera_distance = std::clamp<float>(desired_distance, 0.1, 1000);
-        }
-
-        if (orbit_mode)
-        {
-            camera_rotation_sideways += camera_speed_sideways;
-            camera_rotation_updown = camera_minmax_y * sin(camera_speed_updown * GetTime());
-        }
-        else
-        {
-            // Manual controls:
-
-            if (IsMouseButtonDown(1))
-            {
-                if (previous_mouse_pos.x == -1 && previous_mouse_pos.y == -1)
-                {
-                    previous_mouse_pos = GetMousePosition();
-                }
-                auto movement = Vector2Subtract(GetMousePosition(), previous_mouse_pos);
-
-                camera_rotation_sideways += -movement.x / (GetScreenWidth() / 5.0f);
-                camera_rotation_updown += movement.y / (GetScreenHeight() / 5.0f);
-            }
-        }
-
-        const Quaternion sideways_quaternion = QuaternionFromEuler(0, camera_rotation_sideways, 0);
-        const Vector3 pos_xz = Vector3RotateByQuaternion({ 0, 0, camera_distance }, sideways_quaternion);
-        const Vector3 tangent = Vector3RotateByQuaternion(pos_xz, QuaternionFromEuler(0, -glm::half_pi<float>(), 0));
-        const Quaternion updown_quaternion = QuaternionFromAxisAngle(tangent, camera_rotation_updown);
-        const Vector3 camera_position = Vector3RotateByQuaternion(pos_xz, updown_quaternion);
-
-        camera.position = Vector3Add(camera.target, camera_position);
-
-        previous_mouse_pos = GetMousePosition();
+        UpdateCameraState(camera, camera_state, editor_config);
 
         if (IsKeyPressed(KEY_E))
         {
-            render_gui = !render_gui;
+            editor_config.editing_mode = !editor_config.editing_mode;
+
+            if (editor_config.editing_mode)
+            {
+                SetWindowSize(editor_config.last_width, editor_config.last_height);
+            }
+            else
+            {
+                editor_config.last_width = GetScreenWidth();
+                editor_config.last_height = GetScreenHeight();
+                SetWindowSize(1080, 1350);
+            }
         }
         if (IsKeyPressed(KEY_O))
         {
-            orbit_mode = !orbit_mode;
+            editor_config.orbit_mode = !editor_config.orbit_mode;
         }
 
         BeginDrawing();
@@ -150,7 +182,7 @@ int main()
 
         ImGui::Render();
 
-        if (render_gui)
+        if (editor_config.editing_mode)
         {
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         }
